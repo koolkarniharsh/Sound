9884c4894891810418105ac08b5e78502292d3d2
koolkarniharsh <koolkarniharsh@gmail.com>
2020-08-12 18:58:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+let numberOfButtons = document.querySelectorAll(".drum").length;
+
+for(let i=0; i<numberOfButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        makeSound(this.innerHTML);
+    })
+}
+
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key);
+});
+
+function makeSound(choice){
+    switch(choice){
+        case "w": new Audio("sounds/tom-1.mp3").play();
+            break;
+        case "a": new Audio("sounds/tom-2.mp3").play();
+            break;
+        case "s": new Audio("sounds/tom-3.mp3").play();
+            break;
+        case "d": new Audio("sounds/tom-4.mp3").play();
+            break;
+        case "j": new Audio("sounds/snare.mp3").play();
+            break;
+        case "k": new Audio("sounds/crash.mp3").play();
+            break;
+        case "l": new Audio("sounds/kick-bass.mp3").play();
+            break;
+    }
+}
+// let audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
